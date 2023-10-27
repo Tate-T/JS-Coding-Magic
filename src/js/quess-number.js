0096d13cd728493bca6f4e07764eda9e69cdb5ca
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-10-27 20:46:20 +0300

    quess-number-full

--- src/js/quess-number.js
+++ src/js/quess-number.js
@@ -6,10 +6,14 @@ buttonUserNumber.addEventListener("click", () => {
     const computerRandomNumber = Math.floor(Math.random() * 10) + 1;
  let inputUserNumber2 = inputUserNumber.value 
 
-    if (inputUserNumber2 === computerRandomNumber) {
+    if (inputUserNumber2 == computerRandomNumber) {
         textUserNumber.textContent = `Вітаю, ви вгадали число! ${computerRandomNumber}`
+        textUserNumber.classList.add("win")
+        textUserNumber.classList.remove("loose")
     } else {
         textUserNumber.textContent = `Ви програли, комп’ютер загадав ${computerRandomNumber}`
+        textUserNumber.classList.add("loose")
+        textUserNumber.classList.remove("win")
     }
 })
 
