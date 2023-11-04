79da5fe4a285a619c38f38ea847a38708f1fcbd0
Devohka <110058157+Devohka@users.noreply.github.com>
2023-11-04 13:45:13 +0200

    header-footer-modal#1

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,13 @@
 const btnOn = document.querySelector("[data-open]");
 const btnOff = document.querySelector('[data-close]');
-const beg = document.querySelector(".backdrop-header");
+const beg = document.querySelector(".backdrop");
+
+
+function openModal() {
+    beg.style.display = "block";
+};
+
+btnOn.addEventListener("click", openModal);
 
 
 btnOff.addEventListener("click", closeModal);
