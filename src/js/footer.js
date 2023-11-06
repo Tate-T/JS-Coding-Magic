647ac6125577648971f5ea3f24c1ebd226f865e9
Devohka <110058157+Devohka@users.noreply.github.com>
2023-11-06 18:59:08 +0200

    #1

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,6 +3,7 @@ const btnOff = document.querySelector('[data-close]');
 const beg = document.querySelector(".backdrop");
 
 
+
 function openModal() {
     beg.style.display = "block";
 };
@@ -15,3 +16,4 @@ btnOff.addEventListener("click", closeModal);
 function closeModal() {
     beg.style.display = "none";
 };
+
--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,12 @@
 const btnOn = document.querySelector("[data-open]");
-const btnOff = document.querySelector('[data-close]');
+const btnOff = document.querySelector('[data-close-header]');
 const beg = document.querySelector(".backdrop-header");
+const nameUsir = document.querySelector(".modal__header--comment");
+const textUsir = document.querySelector(".header__hiUser-text");
+
+textUsir.addEventListener("input", (e) => {
+    nameUsir.textContent = "Вітаємо, " + e.currentTarget.value + "!";
+    });
 
 
 btnOff.addEventListener("click", closeModal);
