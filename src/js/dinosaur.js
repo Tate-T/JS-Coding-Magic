8ad5099aa57715cb840abb0ed336678a692e77a1
yasinskislava <yasinskislava@gmail.com>
2023-11-14 19:49:31 +0200

    part 1

--- src/js/dinosaur.js
+++ src/js/dinosaur.js
@@ -1,94 +1,89 @@
-const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("#canvas");
+const main = canvas.getContext("2d");
+const dino = document.querySelector("#dino");
+const cactus = document.querySelector("#cactus");
+const startBtn = document.querySelector("[data-start]");
+let start;
+let sec = 0;
+let x = 600;
+let y = 140;
+let gameOn = false;
+let jumpOn = false;
+let mod = -2;
+let pass = true;
+let jumping;
 
-    const dinoImage = new Image();
-    dinoImage.src = "/src/img/dinosaur.png";
+main.drawImage(dino, 30, y, 60, 60);
 
-    const blockImage = new Image();
-    blockImage.src = "/src/img/image-removebg-preview.png" ; // Змінено на нове зображення
+startBtn.addEventListener("click", () => {
+  if (!gameOn) {
+    start = setInterval(game, 10);
+    gameOn = true;
+  }
+  startBtn.style.display = "none";
+});
 
-    const dino = {
-      x: 50,
-      y: canvas.height - 40,
-      width: 40,
-      height: 40,
-      speed: 5,
-      jumpPower: 10,
-      isJumping: false,
-      image: dinoImage,
-    };
+function game() {
+  sec += 10;
+  if (sec % 2500 === 0) {
+    createCactus();
+    x = 600;
+  }
+  moveCactus();
+  if (jumpOn) {
+    pass = false;
+    jumping = setInterval(jump, 10);
+    jumpOn = false;
+  }
+  gameOver();
+}
 
-    const block = {
-      x: canvas.width,
-      y: canvas.height - 30,
-      width: 30,
-      height: 30,
-      speed: 5,
-      image: blockImage,
-    };
+function createCactus() {
+  main.drawImage(cactus, x, 150, 25, 50);
+}
+function moveCactus() {
+  main.clearRect(x, 150, 25, 50);
+  x-=3;
+  main.drawImage(cactus, x, 150, 25, 50);
+}
 
-    let score = 0;
-    let gameInProgress = true;
-    let speedIncreaseInterval = 5;
-
-    function jump() {
-      if (!dino.isJumping) {
-        dino.isJumping = true;
-        const jumpInterval = setInterval(() => {
-          if (dino.y > canvas.height - 120) {
-            dino.y -= dino.jumpPower;
-          } else {
-            clearInterval(jumpInterval);
-            const fallInterval = setInterval(() => {
-              if (dino.y < canvas.height - 40) {
-                dino.y += dino.jumpPower;
-              } else {
-                clearInterval(fallInterval);
-                dino.isJumping = false;
-              }
-            }, 20);
-          }
-        }, 20);
-      }
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    if (pass) {
+      jumpOn = true;
     }
+    e.preventDefault();
+  }
+});
 
-    function checkCollision() {
-      if (
-        dino.x < block.x + block.width &&
-        dino.x + dino.width > block.x &&
-        dino.y < block.y + block.height
-      ) {
-        gameInProgress = false;
-      }
-    }
+let ms = 0;
+function jump() {
+  ms += 10;
+  if (ms === 1000) {
+    clearInterval(jumping);
+    pass = true;
+    mod = -2;
+    ms = 0;
+  }
+  if (ms === 500) {
+    mod = 2;
+  }
+  main.clearRect(30, y, 60, 60);
+  y += mod;
+  main.drawImage(dino, 30, y, 60, 60);
+}
 
-    function animate() {
-      if (gameInProgress) {
-        requestAnimationFrame(animate);
-      }
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      block.x -= block.speed;
-      if (block.x < -block.width) {
-        block.x = canvas.width;
-        score++;
-      }
-
-      checkCollision();
-
-      ctx.drawImage(block.image, block.x, block.y, block.width, block.height);
-      ctx.drawImage(dino.image, dino.x, dino.y, dino.width, dino.height);
-
-      ctx.fillStyle = "#000";
-      ctx.font = "18px Arial";
-      ctx.fillText("Рахунок: " + score, 10, 20);
-    }
-
-    document.addEventListener("keydown", (event) => {
-      if (event.key === " " && !dino.isJumping) {
-        jump();
-      }
-    });
-
-    animate();
+function gameOver() {
+  if (x <= 85 && x >= 5 && y + 60 >= 150) {
+    main.clearRect(0, 0, 600, 200);
+    gameOn = false;
+    clearInterval(start);
+    clearInterval(jumping);
+    x = 600;
+    y = 140;
+    main.drawImage(dino, 30, y, 60, 60);
+    sec = 0;
+    pass = true;
+    jumpOn = false;
+  }
+}
