33bcf38e2cabe883fc8423dd4cf641c3d7857a82
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-11-16 17:48:03 +0200

    max-number-full

--- src/js/max-number.js
+++ src/js/max-number.js
@@ -1,27 +1,20 @@
-
-let maxNumber1 = document.querySelector("#counter-max-number1");
-let maxNumber2 = document.querySelector("#counter-max-number2");
-let maxNumber3 = document.querySelector("#counter-max-number3");
+const maxNumber1 = document.querySelector("#counter-max-number1");
+const maxNumber2 = document.querySelector("#counter-max-number2");
+const maxNumber3 = document.querySelector("#counter-max-number3");
 
   const text = document.querySelector("#text-redult-max");
 
 
-if (maxNumber1.value == NaN || maxNumber2.value == NaN || maxNumber2.value == NaN) {
-    text.textContent ="gg"
-} else {
-  text.textContent = findMaxNumber()
-}
-
-
-    function findMaxNumber(maxNumber1, maxNumber2, maxNumber3) {
-        return Math.max(maxNumber1.value, maxNumber2.value, maxNumber3.value);
-      
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+        const maxNumber = Math.max(maxNumber1.value, maxNumber2.value, maxNumber3.value);
+    text.textContent = maxNumber;
+    } else {
+        console.log("error");
     }
-
-
-
-
-
+    
+  })
+ 
 
 
 
