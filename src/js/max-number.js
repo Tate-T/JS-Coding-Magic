4d54206d1fc808fe4ea5eb843f23f6cb4aef46c1
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-11-16 17:20:59 +0200

    updete-max-number

--- src/js/max-number.js
+++ src/js/max-number.js
@@ -1,7 +1,7 @@
 
-let maxNumber1 = document.querySelector("#counter-max-number1").value;
-let maxNumber2 = document.querySelector("#counter-max-number2").value;
-let maxNumber3 = document.querySelector("#counter-max-number3").value;
+let maxNumber1 = document.querySelector("#counter-max-number1");
+let maxNumber2 = document.querySelector("#counter-max-number2");
+let maxNumber3 = document.querySelector("#counter-max-number3");
 
   const text = document.querySelector("#text-redult-max");
 
@@ -9,20 +9,17 @@ let maxNumber3 = document.querySelector("#counter-max-number3").value;
 if (maxNumber1.value == NaN || maxNumber2.value == NaN || maxNumber2.value == NaN) {
     text.textContent ="gg"
 } else {
-    findLargestNumber()
+  text.textContent = findMaxNumber()
 }
 
-function findLargestNumber() {
-    console.log(maxNumber1.value);
 
-    maxNumber1 = parseFloat(maxNumber1);
-    maxNumber2 = parseFloat(maxNumber2);
-    maxNumber3 = parseFloat(maxNumber3);
+    function findMaxNumber(maxNumber1, maxNumber2, maxNumber3) {
+        return Math.max(maxNumber1.value, maxNumber2.value, maxNumber3.value);
+      
+    }
 
-    const largestNumber = Math.max(maxNumber1, maxNumber2, maxNumber3);
-    
-    text.textContent = largestNumber;
-}
+
+
 
 
 
